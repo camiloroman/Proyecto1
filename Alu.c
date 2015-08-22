506c8444c735be3624f8be70ad2f65eaa3fdf54c
camiloroman <camiroman0106@gmail.com>
2015-08-22 01:34:20 -0500

    Se creo la funcion Alu.c y se añadio Alu.h

---
+++ Alu.c
@@ -0,0 +1,33 @@
+include <stdio.h>
+#include <stdlib.h>
+#include "Alu.h"
+
+void ADD(unsigned long int *R0,unsigned long int R1,unsigned long int R2)
+{
+    R0=R1+R2;
+}
+
+void AND(unsigned long int *R0,unsigned long int R1,unsigned long int R2)
+{
+    R0=R1&R2;
+}
+
+void EOR(unsigned long int *R0,unsigned long int R1,unsigned long int R2)
+{
+    R0=R1^R2;
+}
+
+void MOV(unsigned long int *R0,unsigned long int R1)
+{
+    R0=R1;
+}
+
+void ORR(unsigned long int *R0,unsigned long int R1,unsigned long int R2)
+{
+    R0=R1|R2;
+}
+
+void SUB(unsigned long int *R0,unsigned long int R1,unsigned long int R2)
+{
+    R0=R1-R2;
+}
